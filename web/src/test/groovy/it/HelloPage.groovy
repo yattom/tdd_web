6ca1c362b4468d0eaf565c752c658f589f6c963e
yattom <tsutomu.yasui@gmail.com>
2013-08-29 18:13:01 +0900

    fixed port aligned with build.gradle

--- web/src/test/groovy/it/HelloPage.groovy
+++ web/src/test/groovy/it/HelloPage.groovy
@@ -2,7 +2,7 @@ package it
 import geb.*
 
 class HelloPage extends Page {
-  static url = "http://localhost:8080/bare-eclipse-web/hello"
+  static url = "http://localhost:8999/bare-eclipse-web/hello"
   static content = {
     本文 { $("body").text() }
   }
