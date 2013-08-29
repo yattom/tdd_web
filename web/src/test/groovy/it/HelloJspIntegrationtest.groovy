16cafa0219e17bd15dbddf8291ec6bedea799819
yattom <tsutomu.yasui@gmail.com>
2013-08-29 19:41:40 +0900

    typo in classname

--- web/src/test/groovy/it/HelloJspIntegrationtest.groovy
+++ web/src/test/groovy/it/HelloJspIntegrationtest.groovy
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
 @RunWith(JUnit4)
-class HelloJspIntegrationtest extends GebReportingTest {
+class HelloJspIntegrationTest extends GebReportingTest {
 
   @Test
   void "挨拶できること"() {
