09f1bd9bb50114db966950beb3dea1b636c2162c
yattom <tsutomu.yasui@gmail.com>
2013-08-30 11:43:54 +0900

    ToDo入力の結合テスト
    まずはページタイトルだけ確認

---
+++ web/src/test/groovy/it/ManageToDoIntegrationTest.groovy
@@ -0,0 +1,24 @@
+package it
+import geb.*
+import geb.junit4.*
+import org.junit.Test
+
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4)
+class ManageToDoIntegrationTest extends GebReportingTest {
+
+  @Test
+  void "入力したToDoが表示できること"() {
+    to NewPage
+    at NewPage
+//    form.with {
+//        todoTile = "朝食"
+//    }
+//    $('input', type: 'submit').click()
+//    
+//    at IndexPage
+//    assert todo == '朝食'
+  }
+}
---
+++ web/src/test/groovy/it/NewPage.groovy
@@ -0,0 +1,10 @@
+package it
+import geb.*
+
+class NewPage extends Page {
+  static url = "http://localhost:8999/tdd_todo/new"
+  static at = { assert title ==  'ToDo入力' }
+  static content = {
+    form { $("form") }
+  }
+}
